c78de647b10c56d084d3bda10268f6f5f6e2d770
janely <janelyapazasencia95@gmail.com>
2026-03-25 23:07:39 -0500

    Implementacion de Interfaces

--- src/acuario/PecesAcuario.kt
+++ src/acuario/PecesAcuario.kt
@@ -1,10 +1,21 @@
 package acuario
+
 abstract class Pez {
     abstract val color: String
 }
-class Tiburon : Pez() {
-    override val color: String = "gris"
+interface AccionPez {
+    fun comer()
 }
-class PezPayaso : Pez() {
+class Tiburon : Pez(), AccionPez {
+    override val color: String = "gris"
+
+    override fun comer() {
+        println("cazar y comer peces")
+    }
+}
+class PezPayaso : Pez(), AccionPez {
     override val color: String = "dorado"
+    override fun comer() {
+        println("comer algas")
+    }
 }
--- src/acuario/main.kt
+++ src/acuario/main.kt
@@ -1,13 +1,42 @@
 package acuario
 
 fun construirAcuario() {
+    val acuario1 = Acuario()
+    acuario1.imprimirTamano()
+
+    val acuario2 = Acuario(ancho = 25)
+    acuario2.imprimirTamano()
+
+    val acuario3 = Acuario(alto = 35, largo = 110)
+    acuario3.imprimirTamano()
+
+    val acuario4 = Acuario(ancho = 25, alto = 35, largo = 110)
+    acuario4.imprimirTamano()
+
+    val acuario5 = Acuario(numeroDePeces = 29)
+    acuario5.imprimirTamano()
+
+    acuario5.volumen = 70
+    acuario5.imprimirTamano()
+
+    val acuario6 = Acuario(largo = 25, ancho = 25, alto = 40)
+    acuario6.imprimirTamano()
+
+    val torre = TanqueTorre(diametro = 25, alto = 45)
+    torre.imprimirTamano()
 }
+
 fun crearPeces() {
     val tiburon = Tiburon()
     val pezPayaso = PezPayaso()
+
     println("Color del tiburon: ${tiburon.color}")
+    tiburon.comer()
+
     println("Color del pez payaso: ${pezPayaso.color}")
+    pezPayaso.comer()
 }
+
 fun main() {
     construirAcuario()
     crearPeces()
